6225fcbb2a01d70cb787bb9e8f7bba153d2bec71
andrewquang <andrew.viquang@gmail.com>
2021-07-16 20:39:10 +0700

    make useStyles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import memories from './images/memories.png';
+import useStyles from './styles';
+// ? import the style for App
 
 const App = () => {
+    const classes = useStyles();
+    // ? defint the class function for style and apply it
+    // ? it will be the same way for style of all components
+    // ? to do style efficiently and independence between components
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography varaint="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
@@ -16,7 +24,7 @@ const App = () => {
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
-                        <Grid item xs={12} sm={7}>
+                        <Grid item xs={12} sm={4}>
                             <Form />
                         </Grid>
                     </Grid>
