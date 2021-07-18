46a9439baeaa270a8e7b92e1eaf3bb1fac05bcc1
andrewquang <andrew.viquang@gmail.com>
2021-07-18 15:44:49 +0700

    setting for deploy

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+dotenv.config();
 
 //const express = require('express');
 // ? in older version we have to do this but
@@ -23,7 +25,11 @@ app.use(cors());
 app.use('/posts', postRoutes);
 // ? above line say that we have to go http://localhost:5000/posts to use {postRoutes}
 
-const CONNECTION_URL ='mongodb+srv://memoryprojectquangchanvi:TdQCzU1SxL44HO8B@cluster0.1min1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.get('/', (req, res) => {
+    res.send('Hello to My API');
+});
+
+//const CONNECTION_URL ='mongodb+srv://memoryprojectquangchanvi:TdQCzU1SxL44HO8B@cluster0.1min1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 // this credential should be secured so this is temporary and put in environment variable
 const PORT = process.env.PORT || 5000;
 
