9aa7915fc89c4713134c3f4f6c10a64522134db9
andrewquang512 <andrew.viquang@gmail.com>
2021-07-22 23:44:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container} from '@material-ui/core';
-import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
+//import { Switch, Route} from 'react-router-dom'
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
